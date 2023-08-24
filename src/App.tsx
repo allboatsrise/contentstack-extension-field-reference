212dc05ce005b785ae96169f6d2097ed3bb9dbd7
Andrej Pavlovic <andrej.pavlovic@massedge.com>
2023-08-24 13:38:20 -0400

    remove initial template styles and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,6 @@
-import { useState } from 'react'
-import {ReactComponent as ReactLogo} from './assets/react.svg'
-import {ReactComponent as ViteLogo} from './assets/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <ViteLogo className="logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <ReactLogo className="logo react" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export const App: React.FC = () => {
+  return 'start coding'
 }
 
-export default App
